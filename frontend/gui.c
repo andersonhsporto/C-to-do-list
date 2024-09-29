fd784e52f2dd78053d6e46471130ea43fb2d4b27
Anderson Porto <anderson.higo2@gmail.com>
2024-09-29 00:34:28 -0300

    Update package.json and GUI window title

--- frontend/gui.c
+++ frontend/gui.c
@@ -88,7 +88,7 @@ void activate(GtkApplication *app, gpointer user_data)
 
 	// Criar a janela principal
 	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), "To-Do List Safada Em  C");
+	gtk_window_set_title(GTK_WINDOW(window), "To-Do List Em  C");
 	gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);
 
 	// Criar um layout de grade
